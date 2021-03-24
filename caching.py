01ad4bf7357bc50c55031dc0332498d776fb212e
alexnesov <alexandrenesovic@gmail.com>
2021-03-24 20:46:35 +0100

    init cach

--- caching.py
+++ caching.py
@@ -2,11 +2,18 @@
 
 """
 Recursive fashion, extremely inefficient.
-Around 31-32 iterations the code starts to halt, it get's extremely slow.
+At around 31-32 iterations the code starts to halt, it get's extremely slow.
 Each number after that is twice as long as the number before.
 
 
 A solution is to remember the values that were computed before.
+"lru" = "least recently used"
+
+
+The data cached is the data that is returned and the data as input parameter (argument)
+Whenever we use the function again with the same arguments it's gonna
+get the data from the storage instead from the function.
+As long as the argument don't change, the result will be instant
 """
 
 """
