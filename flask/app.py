1a00f5376c74c831a1c4d01ea248d576e15f40e4
alexnesov <alexandrenesovic@gmail.com>
2021-03-25 19:44:21 +0100

    flask example

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,63 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template
 
-# create the application object
+
+"""
+# Minimum reproductible example
+
+# Two questions:
+
+1. Should we call function outside route functions? so that we have them available 
+mumtiple route function?
+If no, what is the other better solution?
+
+2.
+"""
 app = Flask(__name__)
 
-# use decorators to link the function to a url
+
+
+def dotest():
+    one = 'one'
+    print('one')
+    return one
+
+
+
+def dotest2():
+    two = 'two'
+    print('two')
+    return two
+
+
+
+def dotest3():
+    three = 'three'
+    print('three')
+    return three
+
+one = str(dotest())
+two = dotest2()
+three = dotest3()
+
 @app.route('/')
 def home():
-    return render_template('welcome.html')  # render a template
+    print('foo')
+    return render_template('welcome.html', 
+    one=one, 
+    two=two, 
+    three=three)  
+
+
+
 
 @app.route('/test_one')
 def welcome():
-    return render_template('test_one.html')  # render a template
+    print('bar')
+    return render_template('test_one.html', 
+    one=one, 
+    two=two, 
+    three=three)
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
